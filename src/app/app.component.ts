3e756a2f1e6b4fa3a1c0ec276bda43285af0a639
Sebastian Carrillo <sebastianulldeveloper@gmail.com>
2021-10-01 07:48:17 -0500

    Primera Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portafolioSebastian';
+export class AppComponent implements OnInit{
+  
+  constructor(
+    private primeNgConf: PrimeNGConfig
+  ){}
+
+  ngOnInit() {
+      this.primeNgConf.ripple = true;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  HttpClientModule } from '@angular/common/http';
+ 
+import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRouterModule,
+    SharedModule,
+    PrimeNgModule,    
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
